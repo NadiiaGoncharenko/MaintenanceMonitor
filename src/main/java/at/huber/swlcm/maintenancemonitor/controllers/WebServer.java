522bef11061ceb987ecd8061b6528bae4cd4a6f0
wi20b083 <wi20b083@technikum-wien.at>
2021-05-14 21:30:11 +0200

    GET hinzugefügt

--- src/main/java/at/huber/swlcm/maintenancemonitor/controllers/WebServer.java
+++ src/main/java/at/huber/swlcm/maintenancemonitor/controllers/WebServer.java
@@ -1,8 +1,17 @@
 package at.huber.swlcm.maintenancemonitor.controllers;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path( "/api/maintenanceMode/" )
 public class WebServer {
+    static String status = "-";
 
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response getPage() {
+        Response.ResponseBuilder rb = Response.ok(status);
+        return rb.header("Access-Control-Allow-Origin", "*").build();
+    }
 }
